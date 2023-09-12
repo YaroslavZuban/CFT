47f5f1c2cbaac60ce1622bbe2524d25c01276ad5
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-09-12 15:46:52 +0700

    оптимизировал алгоритм

--- src/cft/test/zuban/sort_merge/MergeSortFile.java
+++ src/cft/test/zuban/sort_merge/MergeSortFile.java
@@ -3,45 +3,16 @@ package cft.test.zuban.sort_merge;
 import cft.test.zuban.comparer.Compare;
 import cft.test.zuban.line_parser.ArgsParser;
 import cft.test.zuban.work_file.FileHelper;
+import cft.test.zuban.work_file.FileLineReader;
 import cft.test.zuban.work_keys.KeyProcessing;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * В алгоритме реализации:
- * <p>
- * 1. Сначала передаем названия файлов - входного файла для чтения, результирующего файла и временного файла.
- * <p>
- * 2. Используем функцию mergeWithOrder, которая открывает входной файл, считывает строки и передает их в
- * результирующий и временный файлы. Если входной файл пуст или не содержит строк, то операция завершается.
- * <p>
- * <p>
- * 3. Далее переходим к функции mergeSingleLine:
- * <p>
- * Открываем результирующий файл для чтения.
- * Открываем временный файл для записи.
- * Считываем строку из результирующего файла и проверяем, не пуст ли он.
- * Если результирующий файл не пуст:
- * Сравниваем считанную строку и текущую строку.
- * Если условие сравнения истинно, то записываем текущую строку, затем считанную строку. Если условие ложно,
- * просто записываем текущую строку во временный файл.
- * Если ни одна строка не была записана, то записываем текущую строку во временный файл.
- * Если результирующий файл пустой, записываем текущую строку во временный файл.
- * После завершения записи данных, переименовываем и удаляем файлы:
- * <p>
- * 4. Удаляем результирующий файл.
- * Переименовываем временный файл в результирующий файл.
- * Этот алгоритм обеспечивает слияние данных из входного файла в результирующий файл, сохраняя при этом порядок
- */
-
 public class MergeSortFile {
-    private Compare compare;
+    private String pathOut;
     private final String[] args;
-    private boolean isFirstValue = true;
 
     public MergeSortFile(String[] args) {
         this.args = args;
@@ -56,10 +27,9 @@ public class MergeSortFile {
         FileHelper fileHelper = new FileHelper();
         fileHelper.validateFilesExist(filesList);
 
-        compare = new KeyProcessing().identifyTypeSortAndData(keysList);
+        Compare compare = new KeyProcessing().identifyTypeSortAndData(keysList);
 
-        String pathOut = filesList.get(0);
-        String tempFile = "tempFile.txt";
+        pathOut = filesList.get(0);
 
         try {
             createOrClearFile(pathOut);
@@ -68,14 +38,93 @@ public class MergeSortFile {
             System.exit(0);
         }
 
-        for (int i = 1; i < filesList.size(); i++) {
-            try {
-                mergeWithOrder(filesList.get(i), pathOut, tempFile);
-            } catch (IOException e) {
-                System.out.println("Ошибка: в файле" + filesList.get(i) + " не корректные данные.");
-                System.exit(0);
+        try {
+            boolean isSortDecrease = keysList.contains("-d");
+
+            List<FileLineReader> readersList = new ArrayList<>();
+
+            for (int i = 1; i < filesList.size(); i++) {
+                readersList.add(new FileLineReader(filesList.get(i), compare, isSortDecrease));
+            }
+
+            List<String> nonEmptyLines = fetchNonEmptyLines(readersList);
+
+            String currentLine = nonEmptyLines.get(0);
+            int currentIndex = 0;
+
+            while (!isNullsList(nonEmptyLines)) {
+                for (int i = 0; i < nonEmptyLines.size(); i++) {
+                    String line = nonEmptyLines.get(i);
+
+                    if (currentLine == null && line != null) {
+                        currentLine = line;
+                        currentIndex = i;
+                    }
+
+                    if (line != null && compare.isNextValid(currentLine, line)) {
+                        currentLine = line;
+                        currentIndex = i;
+                    }
+                }
+
+                FileLineReader reader = readersList.get(currentIndex);
+
+                if (reader != null) {
+                    String line = reader.readLine();
+
+                    if (line != null && !line.contains(" ") && !line.isEmpty()) {
+                        nonEmptyLines.set(currentIndex, line);
+                    } else {
+                        nonEmptyLines.set(currentIndex, null);
+                        readersList.set(currentIndex, null);
+                    }
+
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(pathOut, true))) {
+                        writer.write(currentLine);
+
+                        if (line != null && line.isEmpty()) {
+                            currentLine = null;
+                        } else {
+                            currentLine = line;
+                        }
+
+                        if (!isNullsList(nonEmptyLines)) {
+                            writer.newLine();
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка. Проверьте данные!");
+            System.exit(0);
+        }
+    }
+
+    private static List<String> fetchNonEmptyLines(List<FileLineReader> readersList) {
+        List<String> lineReaderList = new ArrayList<>();
+
+        for (FileLineReader fileLineReader : readersList) {
+            String line = fileLineReader.readLine();
+
+            if (line != null && !line.isEmpty() && !line.contains(" ")) {
+                lineReaderList.add(line);
+            } else {
+                lineReaderList.add(null);
             }
         }
+
+        return lineReaderList;
+    }
+
+    /*Проверка что в списке одни нули*/
+    private static boolean isNullsList(List<String> list) {
+        for (String line : list) {
+            if (line != null) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -91,62 +140,5 @@ public class MergeSortFile {
             writer.write("");
             writer.close();
         }
-    }
-
-    private void mergeWithOrder(String sourcePath, String destinationPath, String tempPath) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader(sourcePath))) {
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                if (line.contains(" ") || line.isEmpty()) {
-                    break;
-                }
-
-                mergeSingleLine(line, destinationPath, tempPath);
-            }
-        }
-    }
-
-    private void mergeSingleLine(String line, String destinationPath, String tempPath) throws IOException {
-        boolean foundTen = true;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(destinationPath));
-             BufferedWriter writer = new BufferedWriter(new FileWriter(tempPath))) {
-
-            String lineTempFile = reader.readLine();
-
-            while (lineTempFile != null) {
-                if (compare.isNextValid(lineTempFile, line) && foundTen) {
-                    writer.write(line);
-                    writer.newLine();
-                    writer.write(lineTempFile);
-                    foundTen = false;
-                } else {
-                    writer.write(lineTempFile);
-                }
-
-                lineTempFile = reader.readLine();
-
-                if (lineTempFile != null) {
-                    writer.newLine();
-                }
-            }
-        }
-
-        if (foundTen) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempPath, true))) {
-                if (!isFirstValue) {
-                    writer.newLine();
-                }
-
-                writer.write(line);
-                isFirstValue = false;
-            }
-        }
-
-        Path path = Paths.get(destinationPath);
-
-        Files.delete(path);
-        Files.move(Paths.get(tempPath), path);
     }
 }
